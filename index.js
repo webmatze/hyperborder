ecb66e529d4f09384fad67cca958d518997cfdcd
Mathias Karstädt <mathias.karstaedt@gmail.com>
2016-07-18 22:32:55 +0200

    refactoring css

--- index.js
+++ index.js
@@ -1,45 +1,48 @@
 exports.decorateConfig = (config) => {
-  var hyperborderConfig = Object.assign({
-    borderWidth: '4px'
-  }, config.hyperborder);
+  var borderWidth = '4px';
   return Object.assign({}, config, {
-    borderColor: '',
     css: `
-      ${config.css || ''}
-      html, body {
+      html {
         height: 100%;
-      }
-      #mount {
         background-image: linear-gradient(#fc1da7, #fba506);
-        height: 100%;
-        border-radius: ${hyperborderConfig.borderWidth};
+        border-radius: ${borderWidth};
+      }
+      body {
+        position: absolute;
+        top: ${borderWidth};
+        bottom: ${borderWidth};
+        left: ${borderWidth};
+        right: ${borderWidth};
+        border-radius: ${borderWidth}
+      }
+      ${config.css || ''}
+      #mount {
       }
       .hyperterm_main {
+        top: ${borderWidth};
+        bottom: ${borderWidth};
+        left: ${borderWidth};
+        right: ${borderWidth};
         border-width: 0px;
       }
       .hyperterm_main .header_header {
-        top: ${hyperborderConfig.borderWidth};
-        left: ${hyperborderConfig.borderWidth};
-        right: ${hyperborderConfig.borderWidth};
+        top: ${borderWidth};
+        left: ${borderWidth};
+        right: ${borderWidth};
         border-top-left-radius: 4px;
         border-top-right-radius: 4px;
       }
       .hyperterm_main .tabs_list {
         border-bottom-color: ${config.borderColor};
       }
-      .hyperterm_main .tab_active .tab_text {
-        border-left-color: ${config.borderColor};
-        border-right-color: ${config.borderColor};
-      }
       .hyperterm_main .terms_terms {
         border-radius: 0 0 4px 4px;
-        background-color: ${config.backgroundColor};
-        bottom: ${hyperborderConfig.borderWidth};
-        left: ${hyperborderConfig.borderWidth};
-        right: ${hyperborderConfig.borderWidth};
+        bottom: ${borderWidth};
+        left: ${borderWidth};
+        right: ${borderWidth};
       }
       .hyperterm_main .terms_term {
-        margin-top: ${hyperborderConfig.borderWidth};
+        margin-top: ${borderWidth};
       }
     `
   });
