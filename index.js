a86deab4dc752badf409970d6efb0a6138583815
Mathias Karstädt <mathias.karstaedt@gmail.com>
2017-11-16 01:23:41 +0100

    remove console.log call

--- index.js
+++ index.js
@@ -35,7 +35,6 @@ module.exports.onRendererWindow = async (window) => {
   }
 
   if (config.hyperBorder && config.hyperBorder.animate) {
-    console.log('start animation interval');
     window.setInterval(updateBorderAngle, 100);
   }
 
